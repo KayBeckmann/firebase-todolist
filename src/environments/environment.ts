fe72208293327a400eb0908dc614153049504cfb
Kay Beckmann <info@beckmann-md.de>
2023-06-13 12:39:03 +0200

    firebase initialisiert.

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kay-todolist',
+    appId: '1:605031932092:web:90b490666750ae86d0f0b9',
+    storageBucket: 'kay-todolist.appspot.com',
+    apiKey: 'AIzaSyBcUdcmC1qJXAWLJ5ghgTJQczuAJ5ROEQ8',
+    authDomain: 'kay-todolist.firebaseapp.com',
+    messagingSenderId: '605031932092',
+  },
+};
