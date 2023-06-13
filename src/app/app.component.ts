a3ba105980a2922c377d3d5a579c578f29297d61
Kay Beckmann <info@beckmann-md.de>
2023-06-13 13:09:09 +0200

    Firebase auslesen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
+interface Item {
+  name: string,
+
+};
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <ul>
+    <li *ngFor="let item of item$ | async">
+      {{ item.name }}
+    </li>
+  </ul>
+`
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  item$: Observable<any>;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'todos');
+    this.item$ = collectionData(itemCollection);
+  }
 }
